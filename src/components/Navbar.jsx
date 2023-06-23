4edbe6a86bcc236b5cb261e57eafa8d681a55eac
sujitsamaltechworks <sujitofficial294@gmail.com>
2023-06-24 00:50:04 +0530

    updated navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
       <nav className={sticky ? "sticky-nav" : ""}>
         <div className="navbar">
           <Link to="/">
-            <p onClick={goTop}>COINDOM</p>
+            <p onClick={goTop}>COINMASTER</p>
           </Link>
           <ul>
             <li>
